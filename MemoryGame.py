f55b1fed14a3d9c459efbf0db36e2ca12b1446e0
Maze226 <michaelgaragaty@gmail.com>
2023-01-26 07:49:44 +0200

    added content to the game functions

--- MemoryGame.py
+++ MemoryGame.py
@@ -1,14 +1,34 @@
-def generate_sequence():
-    pass
+import random
+import time
+import re
 
 
-def get_list_from_user():
-    pass
+def generate_sequence(diff):
+    generated_list = []
+    for i in range(int(diff)):
+        generated_list.append(random.randint(1, 101))
+    print(generated_list, end='\r')
+    time.sleep(0.7)
+    print('                    ')
+    return generated_list
 
 
-def is_list_equal():
-    pass
+def get_list_from_user(diff):
+    users_list = []
+    for i in range(int(diff)):
+        enter_item = input(f'Please enter the {i + 1} item:')
+        while not re.match(r"^([1-9]|[1-9][0-9]|100|101)$", enter_item):
+            enter_item = input(f'Please enter the {i + 1} item:')
+        users_list.append(int(enter_item))
+    return users_list
 
 
-def play():
-    pass
+def is_list_equal(generated_list, users_list):
+    if generated_list == users_list:
+        return True
+    else:
+        return False
+
+
+def play(diff):
+    print(is_list_equal(generate_sequence(diff), get_list_from_user(diff)))
