724e972ef5a90310ddc3c6a65fed4b21af13b0c4
Maze226 <michaelgaragaty@gmail.com>
2023-02-15 20:55:55 +0200

    added a check to check first if the file exists and then to return the right message

--- MainScores.py
+++ MainScores.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from Utils import SCORES_FILE_NAME
 from Utils import BAD_RETURN_CODE as ERROR
+from os.path import exists
 
 app = Flask('__main__')
 
 
 @app.route('/')
 def show_score():
-    with open(SCORES_FILE_NAME, 'r', encoding='utf8') as file:
-        if file:
-            score = file.readline()
+    file = SCORES_FILE_NAME
+    if exists(file):
+        with open(file, 'r', encoding='utf8') as score_file:
+            score = score_file.readline()
             return f'''<html>
                        <head>
                            <title>Scores Game</title>
@@ -19,18 +21,18 @@ def show_score():
                                <div id="score">{score}</div>
                            </h1>
                        </body>
-                       </html>'''
-        else:
-            return f'''<html> 
-                       <head>
-                           <title>Scores Game</title>
-                       </head>
-                       <body>
-                           <h1>
-                               <div id="score" style="color:red">{ERROR}</div>
-                           </h1>
-                       </body>
-                       </html>'''
+                       </html>''', 200
+    else:
+        return f'''<html> 
+                   <head>
+                       <title>Scores Game</title>
+                   </head>
+                   <body>
+                       <h1>
+                           <div id="score" style="color:red">{ERROR}</div>
+                       </h1>
+                   </body>
+                   </html>''', ERROR
 
 
 if __name__ == '__main__':
