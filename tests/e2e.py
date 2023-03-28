81e1e37348acbd6056ab3b06d956496a3769e6c4
Maze226 <michaelgaragaty@gmail.com>
2023-03-28 18:18:58 +0300

    changed failed test exit code to -1

--- tests/e2e.py
+++ tests/e2e.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 
 def test_scores_service():
     web_driver = webdriver.Chrome(service=Service('$(pwd)/tests/chromedriver'))
-    web_driver.get('http://localhost:5000')
+    web_driver.get('http://localhost:8777')
     score = web_driver.find_element(By.ID, 'score').text
     if 1 <= int(score) <= 1000:
         return True
@@ -20,7 +20,7 @@ def main_function():
     if test_scores_service():
         sys.exit(0)
     else:
-        sys.exit(1)
+        sys.exit('-1')
 
 
 main_function()
