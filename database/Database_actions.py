448ad33126d412949965c1c9332bafee909bb147
Michael Garagaty <michaelgaragaty@gmail.com>
2023-06-06 09:36:46 +0000

    added database actions for database manipulations

---
+++ database/Database_actions.py
@@ -0,0 +1,48 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='wog-score-data',
+    port='3306',
+    database="games"
+)
+connect = db.connect
+db_cursor = db.cursor()
+
+
+def create_user(name):
+    db.connect()
+    query='INSERT INTO users_scores (name, score) VALUES (%s, %s)'
+    val=(name, 0)
+    db_cursor.execute(query, val)
+    db.commit()
+    db.close()
+    
+    
+def get_user(name):
+    db.connect()
+    query = 'SELECT score FROM users_scores WHERE name= %s'
+    val = (name,)
+    db_cursor.execute(query, val)
+
+    myresult = db_cursor.fetchall()
+
+    return myresult
+
+def get_all():
+    db.connect()
+    query='SELECT * FROM users_scores'
+    db_cursor.execute(query)
+    result = db_cursor.fetchall()
+    db.close()
+    return result
+
+
+def update_score(score, name):
+    db.connect()
+    query="UPDATE users_scores SET score = %s WHERE name = %s"
+    val=(score, name)
+    db_cursor.execute(query, val)
+    db.commit()
+    db.close()
