903d552edfd08ea5aa0df447e87e0a7d4bcaf1a1
Michael Garagaty <michaelgaragaty@gmail.com>
2023-06-10 07:48:58 +0000

    fixed the check in show_score and updated functions names

--- MainScores.py
+++ MainScores.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 from Utils import BAD_RETURN_CODE as ERROR
-from database.Database_actions import get_all, get_user, db
+from database.Database_actions import get_user, db
 
 app = Flask('__main__')
 
 
 @app.route('/')
 def show_score(name):
-    if db.connect():
+    if db:
         return render_template('/my_score.html', game_score=get_user(name)), 200
     else: 
         return render_template('error.html', error=ERROR), ERROR
