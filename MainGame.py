d43c4a7916458d11c0a5be1c020fc0e180ff1f17
Michael Garagaty <michaelgaragaty@gmail.com>
2023-06-10 07:50:48 +0000

    removed MainScores import and updated database actions functions names

--- MainGame.py
+++ MainGame.py
@@ -1,12 +1,11 @@
 from Live import load_game, welcome
-from database.Database_actions import create, get_user
-from MainScores import show_score
+from database.Database_actions import create_user, get_user
 
 while True:
     name = input("What is your name?")
     check = get_user(name)
     if check is None and name != '':
-        create(name)
+        create_user(name)
         break
 
 
