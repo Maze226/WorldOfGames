1383e3975bcf203625b34c22f6a6de8084890e28
Michael Garagaty <michaelgaragaty@gmail.com>
2023-06-10 07:47:36 +0000

    updated functions names

--- Score.py
+++ Score.py
@@ -1,8 +1,8 @@
-from database.Database_actions import get_score, update
+from database.Database_actions import get_user, update_score
 
 
 def add_score(difficulty, username):
     print(difficulty)
     score = (difficulty * 3) + 5
-    update(score, username)
-    return get_score(username)
+    update_score(score, username)
+    return get_user(username)
